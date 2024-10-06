b76a4d07fd1344421d5e123216c4a9d24e532b15
markiskino35 <markiskino35@gmail.com>
2024-10-06 18:54:20 +0800

    feat: consume api and conversion rate logic

--- src/components/ConverterForm.jsx
+++ src/components/ConverterForm.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CurrencySelect from './CurrencySelect';
 
 const ConverterForm = () => {
+  const [amount, setAmount] = useState(100);
   const [fromCurrency, setFromCurrency] = useState('USD');
   const [toCurrency, setToCurrency] = useState('MYR');
+  const [result, setResult] = useState('');
 
   // Swap the values of fromCurrency and toCurrency
   const handleSwapCurrency = () => {
@@ -11,11 +13,42 @@ const ConverterForm = () => {
     setToCurrency(fromCurrency);
   };
 
+  // Function to fetch the exchange rate and update the result
+  const getExchangeRate = async () => {
+    const API_KEY = import.meta.env.VITE_API_KEY;
+    const API_URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/pair/${fromCurrency}/${toCurrency}`;
+
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) throw Error('Something went wrong!');
+
+      const data = await response.json();
+      const rate = (data.conversion_rate * amount).toFixed(2);
+      setResult(`${amount} ${fromCurrency} = ${rate} ${toCurrency}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    getExchangeRate();
+  };
+
+  // Fetch exchange rate on initial render
+  useEffect(() => getExchangeRate, []);
+
   return (
-    <form className='converter-form'>
+    <form className='converter-form' onSubmit={handleFormSubmit}>
       <div className='form-group'>
         <label className='form-label'>Enter amount</label>
-        <input type='number' className='form-input' required />
+        <input
+          type='number'
+          className='form-input'
+          required
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </div>
 
       <div className='form-group form-currency-group'>
@@ -51,7 +84,7 @@ const ConverterForm = () => {
       <button type='submit' className='submit-button'>
         Get Exchange Rate
       </button>
-      <p className='exchange-rate-result'>1000 USD = 1000 MYR</p>
+      <p className='exchange-rate-result'>{result}</p>
     </form>
   );
 };
