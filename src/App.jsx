98699f7161b4fcf2bcb0782dc49804aee6b3f564
markiskino35 <markiskino35@gmail.com>
2024-10-07 00:27:19 +0800

    feat: add app title and description

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import ConverterForm from './components/ConverterForm';
 const App = () => {
   return (
     <div className='currency-converter'>
-      <h2 className='converter-title'>Currency Converter</h2>
+      <h2 className='converter-title'>FlipCash</h2>
+      <p className='converter-description'>
+        Fast and efficient currency conversion
+      </p>
       <ConverterForm />
     </div>
   );
